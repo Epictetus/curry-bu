7bfc9e10eb86662b6ccb12e210fd56256ef59057
chocoby <chocoby@gmail.com>
2012-02-11 04:10:05 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Currybu::Application.config.secret_token = 'b321ce722f16ee182828662d0201d1a304939c974ddb66e182c0130d98485ef03fe878030b61468b56d9f5f5eeb8915c2557850934920f9e96f54849b8bcf31b'
