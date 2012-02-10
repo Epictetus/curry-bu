d44f34f7c6249157bc9fd93d0b13f21c325c7df8
chocoby <chocoby@gmail.com>
2012-02-11 05:05:31 +0900

    ユーザー認証周りを作った

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '3.2.1'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+gem 'mysql2'
 
 
 # Gems used only for assets and not required
@@ -21,6 +21,8 @@ group :assets do
 end
 
 gem 'jquery-rails'
+
+gem 'devise'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable
+
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :password, :password_confirmation, :remember_me
+end
