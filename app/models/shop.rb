0050bb0d442001286cb583a331fe0638b074b96b
chocoby <chocoby@gmail.com>
2012-02-16 23:30:12 +0900

    お店を登録/更新した人のユーザー名を出すようにした

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,6 +3,6 @@ class Shop < ActiveRecord::Base
     presence: true,
     uniqueness: true
 
-  #belongs_to :user, class_name: "CreateUser", foreign_key: "create_user_id"
-  #belongs_to :user, class_name: "UpdateUser", foreign_key: "update_user_id"
+  belongs_to :create_user, class_name: "User", foreign_key: "create_user_id"
+  belongs_to :update_user, class_name: "User", foreign_key: "update_user_id"
 end
