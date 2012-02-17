fb854bb69fe78d32b1899d2d7d76eb8d1c42c20e
chocoby <chocoby@gmail.com>
2012-02-17 12:44:04 +0900

    migration 修正

--- db/migrate/20120216013254_add_user_to_shops.rb
+++ db/migrate/20120216013254_add_user_to_shops.rb
@@ -1,6 +1,6 @@
 class AddUserToShops < ActiveRecord::Migration
   def change
-    add_column :shops, :create_user_id, :integer, null: false, default: ""
-    add_column :shops, :update_user_id, :integer, null: false, default: ""
+    add_column :shops, :create_user_id, :integer, null: false
+    add_column :shops, :update_user_id, :integer, null: false
   end
 end
