7673757949480217313fa17bed05f776593e7dc4
Kenta Okamoto <okamoto@fact-real.com>
2012-04-25 18:28:28 +0900

    Item model のテストをいくつか追加

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Item do
+  describe "Associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:shop) }
+  end
+
+  describe "Valiadtion" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:image) }
+    it { should validate_presence_of(:ate_at) }
+    it { should validate_presence_of(:shop) }
+    it { should validate_numericality_of(:price).only_integer }
+  end
+
+  describe "Scope" do
+    it { Item.should respond_to :ate_new }
+  end
+end
