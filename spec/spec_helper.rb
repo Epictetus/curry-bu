3bcc4dafce861b758800eb19620209f286f29a19
chocoby <chocoby@gmail.com>
2012-05-10 00:50:33 +0900

    capybara ログイン処理ヘルパー作成

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  require 'capybara/rspec'
   require 'factory_girl'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
@@ -27,6 +28,9 @@ Spork.prefork do
     # config.mock_with :mocha
     # config.mock_with :flexmock
     # config.mock_with :rr
+    config.mock_with :rspec
+
+    config.include LoginMacros
 
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
