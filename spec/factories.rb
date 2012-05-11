fabce283b271a90be861345e82cb8d9606f2e913
chocoby <chocoby@gmail.com>
2012-05-11 15:23:07 +0900

    requests の shop spec(まだまだ途中)

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,15 @@
 FactoryGirl.define do
+
+  factory :user do
+    username "test user"
+    password "test user"
+    password_confirmation "test user"
+  end
+
   factory :shop do
     name "test shop"
     create_user_id 1
     update_user_id 1
   end
+
 end
