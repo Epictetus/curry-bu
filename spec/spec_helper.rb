b0affdeee0df891b7944df294a3377e45921064e
chocoby <chocoby@gmail.com>
2012-05-29 00:11:27 +0900

    テスト時に CarrierWave が使用するディレクトリを変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,15 +28,38 @@ Spork.prefork do
       DatabaseCleaner.strategy = :transaction
     end
 
+    config.before(:all) do
+      if defined?(CarrierWave)
+        CarrierWave::Uploader::Base.descendants.each do |klass|
+          next if klass.anonymous?
+          klass.class_eval do
+            def cache_dir
+              "#{Rails.root}/spec/support/uploads/tmp"
+            end
+
+            def store_dir
+              "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+            end
+          end
+        end
+      end
+    end
+
     config.before(:each) do
       DatabaseCleaner.start
+    end
+
+    config.after(:all) do
+      if Rails.env.test?
+        FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+      end
     end
 
     config.after(:each) do
       DatabaseCleaner.clean
     end
-  end
 
+  end
 end
 
 Spork.each_run do
