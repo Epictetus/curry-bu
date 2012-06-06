26623a7c3d00d56dce77fbfcdba64a512f32194c
chocoby <chocoby@gmail.com>
2012-06-06 10:08:28 +0900

    画像投稿時に画像をリサイズし、サムネイルを生成する #64
    オリジナルの画像は残しておき、大きいサイズ(最大 700px x 700px)と
    小さいサイズ(最大 300px x 300px)の2つのバージョンを生成するようにした

--- app/uploaders/item_uploader.rb
+++ app/uploaders/item_uploader.rb
@@ -1,12 +1,22 @@
 # encoding: utf-8
 class ItemUploader < CarrierWave::Uploader::Base
 
+  include CarrierWave::MiniMagick
+
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :normal do
+    process resize_to_fit: [700, 700]
+  end
+
+  version :thumb do
+    process resize_to_fit: [300, 300]
   end
 
   def extension_white_list
